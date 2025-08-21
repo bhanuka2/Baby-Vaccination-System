2c3959f4965e9866ffd565ec709c4a6cf2704cbf
bhanuka2 <bhanukashashiindra.com>
2025-08-21 22:27:16 +0530

    feat: implement navbar

--- src/assets/components/Common/Header.tsx
+++ src/assets/components/Common/Header.tsx
@@ -1,29 +1,60 @@
 import React from 'react';
-import '../../../styles/Header.css';
 
-const Header: React.FC = () => {
+
+interface HeaderProps {
+  showDashboardButtons?: boolean;
+}
+
+const Header: React.FC<HeaderProps> = ({ showDashboardButtons = false }) => {
   return (
-    <div className="header-container">
-      {/* Background bar */}
-      <div className="header-background" />
-      
-      {/* Logo */}
-      <img 
-        className="header-logo" 
-        src="https://api.builder.io/api/v1/image/assets/TEMP/b14bd17cd73b3500f16d387463f2e8366bfb2fd2?width=180" 
-        alt="ABC Logo" 
+    <header className="header-container" style={{ position: 'relative', width: '100%', height: '100px', zIndex: 100 }}>
+      <div className="header-background"></div>
+
+      <img
+        className="header-logo" style={{marginTop: '5px'}}
+        src="https://api.builder.io/api/v1/image/assets/TEMP/131b92fa2f7aaddceda632a21b86a6a5fc310f93?width=180"
+        alt="ABC Logo"
+        loading="eager"
       />
-      
-      {/* Vaccination button */}
-      <div className="vaccination-button">
-        <span className="vaccination-text">Vaccination</span>
-      </div>
-      
-      {/* Login button */}
-      <div className="login-button">
-        <span className="login-text">Log In</span>
-      </div>
-    </div>
+
+      {showDashboardButtons ? (
+        <>
+          <button
+            className="vaccination-button"
+            type="button"
+            aria-label="Profile"
+          >
+            <span className="vaccination-text">Profile</span>
+          </button>
+
+          <button
+            className="login-button"
+            type="button"
+            aria-label="Logout"
+          >
+            <span className="login-text">Logout</span>
+          </button>
+        </>
+      ) : (
+        <>
+          <button
+            className="vaccination-button"
+            type="button"
+            aria-label="Vaccination information"
+          >
+            <span className="vaccination-text">Vaccination</span>
+          </button>
+
+          <button
+            className="login-button"
+            type="button"
+            aria-label="Sign up for account"
+          >
+            <span className="login-text">Sign Up</span>
+          </button>
+        </>
+      )}
+    </header>
   );
 };
 
